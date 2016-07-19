1549f75d44b6114aca78b2d58cb7299cdcc57da8
Nik Polyakov <Nikita.Polyakov@noveogroup.com>
2016-07-19 11:26:35 +0600

    Initial commit

---
+++ app/src/main/java/com/school/noveo/notifications/MainActivity.java
@@ -0,0 +1,25 @@
+package com.school.noveo.notifications;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+    }
+
+}
