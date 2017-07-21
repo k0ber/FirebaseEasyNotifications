51c04eac76d115013b49cacb5941359e0ee3728d
Nik Polyakov <Nikita.Polyakov@noveogroup.com>
2017-07-21 15:48:21 +0700

    add topic subscription

--- app/src/main/java/com/school/noveo/notifications/MainActivity.java
+++ app/src/main/java/com/school/noveo/notifications/MainActivity.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final String FCM_ALL_TOPIC = "All";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic(FCM_ALL_TOPIC);
+
+        Log.d(TAG, "Token: " + FirebaseInstanceId.getInstance().getToken());
 
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
